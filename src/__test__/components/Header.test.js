0a0cbc6a318d358deba112e778658c04721b9a32
Luis Locon <luis.locon@gmail.com>
2022-05-20 23:27:03 -0600

    creating snapshots

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import ProviderMock from '../../__mocks__/ProviderMock';
 import Header from '../../components/Header';
+import { create } from 'react-test-renderer';
 
 describe('<Header />', () => {
   // Con shallow puedo usar dos componentes y probarlo como uno solo
@@ -22,5 +23,16 @@ describe('<Header />', () => {
     );
 
     expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header Snapshot', () => {
+  test('Comprobar la UI del componente', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
